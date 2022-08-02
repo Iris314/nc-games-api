c74908fe853b166dfae0c04cd4babfb0b02ca97c
iris <25711391+Iris314@users.noreply.github.com>
2022-08-02 11:31:55 +0100

    change behaviour of invalid key in patch reviews request

--- models/games.model.js
+++ models/games.model.js
@@ -1,5 +1,36 @@
 const db = require("../db/connection");
 
 exports.selectCategories = () => {
-  return db.query("SELECT * FROM categories").then(({ rows }) => rows);
+  return db.query("SELECT * FROM categories;").then(({ rows }) => rows);
+};
+
+exports.selectReviewById = (reviewId) => {
+  return db
+    .query(`SELECT * FROM reviews WHERE review_id = $1;`, [reviewId])
+    .then(({ rows }) => {
+      if (rows.length === 0) {
+        return Promise.reject({ code: 404, msg: "review not found" });
+      }
+      return rows[0];
+    });
+};
+
+exports.changeReviewById = (reviewId, votes) => {
+  if (votes) {
+    return db
+      .query(
+        `UPDATE reviews SET votes = votes + $1 WHERE review_id = $2 Returning*;`,
+        [votes, reviewId]
+      )
+      .then(({ rows }) => {
+        if (rows.length === 0) {
+          return Promise.reject({ code: 404, msg: "review not found" });
+        }
+        return rows[0];
+      });
+  } else {
+    return db
+      .query(`SELECT * FROM reviews WHERE review_id = $1;`, [reviewId])
+      .then(({ rows }) => rows[0]);
+  }
 };
