f219814042f77e7526766f6e722dbaebf90b9be2
iris <25711391+Iris314@users.noreply.github.com>
2022-08-02 15:08:48 +0100

    remove repetition in query string

--- models/games.model.js
+++ models/games.model.js
@@ -7,7 +7,7 @@ exports.selectCategories = () => {
 exports.selectReviews = () => {
   return db
     .query(
-      `SELECT owner, title, reviews.review_id, review_img_url, reviews.votes, category, owner, reviews.created_at, designer,
+      `SELECT title, reviews.review_id, review_img_url, reviews.votes, category, reviews.owner, reviews.created_at, designer,
       COUNT(comment_id) AS comment_count FROM reviews 
       LEFT JOIN comments ON comments.review_id = reviews.review_id
       GROUP BY reviews.review_id`
