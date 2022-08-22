5bdde1c3a8a374d787f912ff1b9682a4dd8a3add
iris <25711391+Iris314@users.noreply.github.com>
2022-08-22 09:58:29 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const apiRouter = require("./routes/api-router");
+const cors = require("cors");
 
 const app = express();
-app.use(express.json());
 
-const apiRouter = require("./routes/api-router");
+app.use(express.json());
 app.use("/api", apiRouter);
+app.use(cors());
 
 app.all("*", (req, res) => {
   res.status(404).send({ msg: "URL path not found" });
